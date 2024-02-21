daa37da1deefd145ae41b6b3a60c4f5e268b101d
Evangelos Tripolitakis <vtripolitakis@me.com>
2024-02-21 10:05:02 +0200

    [CHG] added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@
 import "styles/globals.css"
 import 'highlight.js/styles/docco.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+  <Component {...pageProps} />
+  <Analytics />
+  </>)
 }
