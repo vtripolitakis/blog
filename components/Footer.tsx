4bfebe944ba01bdddfc170505b010f8a6bb27c3b
Vangelis Tripolitakis <vtripolitakis@gmail.com>
2025-11-12 19:54:42 +0200

    [CHG] minor changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="text-center mt-8">
-      Crafted with &#10084; in Crete. &copy; 2022-2024
+      Crafted with &#10084; in Crete. &copy; 2022-2025
     </div>
   );
 };
